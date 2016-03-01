5f51bc36807873a012b2fa54569a46f01abd9495
biler tan <qi.tan@foxmail.com>
2016-03-01 17:38:32 +0800

    Initial Commit

---
+++ refreshEasyTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  refreshEasyTest
+//
+//  Created by qitan on 16/3/1.
+//  Copyright © 2016年 qitan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
